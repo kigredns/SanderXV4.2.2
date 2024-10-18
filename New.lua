3bdcb8be853b0819f627dcf01010b36dd089eec4
DeveloperArabHub <mikolajkim585@gmail.com>
2024-10-18 17:08:46 +0200

    Update New.lua

--- New.lua
+++ New.lua
@@ -124,9 +124,9 @@ end)
 -- Wywołanie skryptów po kliknięciu ImageButton:
 
 ImageButton.MouseButton1Click:Connect(function()
-     loadstring(game:HttpGet('https://raw.githubusercontent.com/kigredns/SanderXV4.2.2/refs/heads/main/sanderxvipSS.lua'))()
+     loadstring(game:HttpGet('https://raw.githubusercontent.com/kigredns/SanderXV4.2.2/refs/heads/main/NormalSS.lua'))()
 end)
 
 ImageButton_2.MouseButton1Click:Connect(function()
-    loadstring(game:HttpGet('https://pastebin.com/raw/Skrypt2'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/kigredns/SanderXV4.2.2/refs/heads/main/sanderxvipSS.lua'))()
 end)
